0cb11ad0e1f98c91eff401c793cc8dbd768f3037
yuudong123 <1mouri81@gmail.com>
2023-03-24 17:46:52 +0900

    add page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [minutes, setMinutes] = useState("");
+  const [disable, setDisable] = useState(true);
+
+  const reverseInput = () => {
+    setDisable((prev) => !prev);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>단위 환산기</h1>
+      <p>시간을 분으로 환산해주는 리액트 앱</p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        {disable ? (
+          <>
+            <div>
+              <input type="number" placeholder="minute" value={minutes} onChange={(e) => setMinutes(e.target.value)} disabled={!disable} />
+              <label htmlFor="">minutes</label>
+            </div>
+            <div>
+              <input type="number" placeholder="hour" value={minutes / 60} disabled={disable} />
+              <label htmlFor="">hours</label>
+            </div>
+          </>
+        ) : (
+          <>
+            <div>
+              <input type="number" placeholder="hour" value={minutes / 60} disabled={disable} />
+              <label htmlFor="">hours</label>
+            </div>
+            <div>
+              <input type="number" placeholder="minute" value={minutes} onChange={(e) => setMinutes(e.target.value)} disabled={!disable} />
+              <label htmlFor="">minutes</label>
+            </div>
+          </>
+        )}
+      </form>
+      <button onClick={reverseInput}>반전</button>
     </div>
   );
 }
